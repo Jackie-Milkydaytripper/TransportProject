a38562c86fc800c1003dd3805a2491b7c1a85da6
Jackie-Milkydaytripper <31907958+Jackie-Milkydaytripper@users.noreply.github.com>
2017-10-22 22:31:52 -0400

    Add files via upload

---
+++ Assets/SummonCubes.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SummonCubes : MonoBehaviour {
+
+	public GameObject CubeSummon;
+	public GameObject currentCube;
+	Vector3 cubePosition;
+	int xPosition;
+
+	// Use this for initialization
+
+	void Start () {
+
+		cubePosition = new Vector3 (-15, 0, 0);
+		xPosition += 2;
+		for (int i = 0; i < 16; i++){
+
+		currentCube = Instantiate (CubeSummon, cubePosition, Quaternion.identity);
+		currentCube.GetComponent<Renderer> ().material.color = Color.white;
+		cubePosition += new Vector3 (xPosition, 0, 0);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+			
+		}
+
+
+
+		
+	}
