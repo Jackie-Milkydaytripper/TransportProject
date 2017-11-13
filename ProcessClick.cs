5b92bf0dc4ee95125ad7a108dd308e7d6f6cbab2
Jackie-Milkydaytripper <31907958+Jackie-Milkydaytripper@users.noreply.github.com>
2017-11-13 01:23:12 -0500

    Add files via upload

---
+++ ProcessClick.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProcessClick : MonoBehaviour {
+	public int MyX, MyY;
+	//SummonCubes mySummonCubes;
+	// Use this for initialization
+	void Start () {
+		//mySummonCubes = GameObject.Find ("CubeSummon").GetComponent<SummonCubes> ();
+		}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnMouseDown() {
+		SummonCubes.ProcessClick (gameObject, MyX, MyY);
+		//print ("x" + x + "...y: " + y); 
+		}
+		
+}
